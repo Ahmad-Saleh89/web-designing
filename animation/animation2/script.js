eeab7488eb9d88d56ff8bd673183ab083ea63325
Ahmad-Sy <ahmadsaleh1989@gmail.com>
2017-07-26 15:15:42 -0500

    Sending message visual effect

--- animation/animation2/script.js
+++ animation/animation2/script.js
@@ -4,4 +4,12 @@ $(document).ready(function(){
     $('.OnOff').toggleClass('toRed');
     $('.extended').toggleClass('triggered');
   });
+
+  $('#send-btn').on('click', function(){
+    $('#msg-content').addClass('sent');
+    setTimeout(function(){
+   $('#msg-content').addClass('appear').removeClass('sent');
+ }, 1400);
+  $('#msg-content').removeClass('appear');
+  });
 });
