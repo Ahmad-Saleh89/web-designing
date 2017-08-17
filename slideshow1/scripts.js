aae764f5d7e560ebed45d91091928d58961be49e
Ahmad-Sy <ahmadsaleh1989@gmail.com>
2017-08-16 23:19:12 -0500

    Auto slide show
    Using setInterval method to create an auto slide show

--- slideshow1/scripts.js
+++ slideshow1/scripts.js
@@ -1,15 +1,31 @@
 $(document).ready(function(){
   var $firstSlide = $('.slides li:nth-child(1)');
   var $lastSlide = $('.slides li:nth-child(5)');
+  var $slider = $('.img-slider');
+  var interval;
 
-  $('.next-icon').click(function(){
+  function startSlider(){
+    interval = setInterval(slideLeft ,5000);
+  }
+  function stopSlider(){
+    clearInterval(interval);
+  }
+  $slider.on('mouseenter', stopSlider).on('mouseleave', startSlider);
+  // Start the slider when the page is loaded:
+  startSlider();
+
+  // Next & Prev buttons functionality:
+  $('.next-icon').click(slideLeft);
+  $('.prev-icon').click(slideRight);
+
+  function slideLeft(){
     var $currentSlide = $('.current');
     var $nextSlide = $currentSlide.next();
 
     if($nextSlide.length){
       $currentSlide.addClass('slide-current-left');
       $nextSlide.addClass('slide-left');
-    }else{
+    }else{  // go back to the first slide:
       $currentSlide.removeClass('current').addClass('slide-current-left');
       $firstSlide.addClass('slide-left current');
       setTimeout(function(){
@@ -20,16 +36,16 @@ $(document).ready(function(){
       $currentSlide.removeClass('current slide-current-left');
       $nextSlide.addClass('current').removeClass('slide-left');
     },1000);
-  });
+  }
 
-  $('.prev-icon').click(function(){
+  function slideRight(){
     var $currentSlide = $('.current');
     var $prevSlide = $currentSlide.prev();
 
     if($prevSlide.length){
       $currentSlide.addClass('slide-current-right');
       $prevSlide.addClass('slide-right');
-    }else{
+    }else{ // go back to the last slide:
       $currentSlide.removeClass('current').addClass('slide-current-right');
       $lastSlide.addClass('slide-right current');
       setTimeout(function(){
@@ -40,5 +56,6 @@ $(document).ready(function(){
       $currentSlide.removeClass('current slide-current-right');
       $prevSlide.addClass('current').removeClass('slide-right');
     },1000);
-  });
+  }
 });
+
