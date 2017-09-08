ee6a02e0844926d777d3bbd6dad273acbc12e100
ahmad saleh <ahmadsaleh1989@gmail.com>
2017-09-07 22:09:52 -0500

    Parallax effect

--- parallax/scripts.js
+++ parallax/scripts.js
@@ -12,5 +12,14 @@ $(document).ready(function(){
     $('.fore-bird').css({
       'transform' : 'translateY(-'+wScroll/30+'%)'
     });
+
+    if(wScroll > $('.clothes-pics').offset().top - $(window).height() / 1.4){
+      $('.clothes-pics figure').each(function(i){
+
+        setTimeout(function(){
+          $('.clothes-pics figure').eq(i).addClass('is-showing');
+        }, 150 * (i+1));
+      })
+    }
   });
 });
