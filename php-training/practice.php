dfabaa9e606271940321008bcd4f8e8dca6b5013
ahmad saleh <ahmadsaleh1989@gmail.com>
2017-10-02 21:20:52 -0500

    some styles

--- php-training/practice.php
+++ php-training/practice.php
@@ -8,7 +8,7 @@
   <head>
     <meta charset="utf-8">
     <title>More php</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet2" href="styles.css">
   </head>
   <body>
     <h1>More Php</h1>
@@ -23,6 +23,17 @@
           echo "$x is equal to $y";
         }
        ?>
+    </div>
+
+    <div class="wrapper">
+      <h2>String Operators:</h2>
+      <ul>
+        <li>The <span>"."</span> operator combines teh value of the right
+        argument with the left argument, like this:</li>
+
+        <li>The <span>". ="</span> operator appends the value in the right
+        with the value in the left, like this:</li>
+      </ul>
     </div>
   </body>
 </html>
